0e5ad3a76508a3ea14132d53c65c63dce69a0d53
wuforde <david.forde@gmail.com>
2024-10-13 20:46:48 -0500

    added cards remaining

--- WuDeckLib/obj/Debug/net8.0/WuDeckLib.AssemblyInfo.cs
+++ WuDeckLib/obj/Debug/net8.0/WuDeckLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WuDeckLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f21a8c0bf32ab7cb319f53bf75e0e5ea6e7d3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729b9ae709f4ce8af7b907c7d234ce4bcacb2ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WuDeckLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WuDeckLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
